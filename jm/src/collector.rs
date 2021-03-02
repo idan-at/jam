79c6eb0ccb3154b86cab852a6831523b7b71adab
Idan Attias <idana@wix.com>
2021-03-02 11:38:34 +0200

    complete collector test

--- jm/src/collector.rs
+++ jm/src/collector.rs
@@ -17,27 +17,36 @@ mod tests {
 
   #[test]
   fn collects_all_packages_dependencies() {
+    let dep1 = Dependency {
+      name: "dep1",
+      real_name: "dep1",
+      version_or_dist_tag: "latest"
+    };
+    let dep2 = Dependency {
+      name: "dep1",
+      real_name: "dep1",
+      version_or_dist_tag: "latest"
+    };
+
     let packages = vec![{
       Package {
         name: "p1".to_string(),
         version: "1.0.0".to_string(),
-        dependencies: vec![Dependency {
-          name: "dep1",
-          real_name: "dep1",
-          version_or_dist_tag: "latest"
-        }],
+        dependencies: vec![dep1],
         dev_dependencies: vec![],
       },
       Package {
         name: "p2".to_string(),
         version: "1.0.0".to_string(),
-        dependencies: vec![Dependency {
-          name: "dep1",
-          real_name: "dep1",
-          version_or_dist_tag: "latest"
-        }],
+        dependencies: vec![dep2],
         dev_dependencies: vec![],
       }
-    }]
+    }];
+
+    let collector = Config::new(packages);
+
+    let expected = vec![dep1, dep2];
+
+    assert_eq!(collector.collect(), expected);
   }
 }
