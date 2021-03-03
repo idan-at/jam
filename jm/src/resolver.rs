161532cb2acfbb33ccf8aa14016727f50b627d3d
Idan Attias <idana@wix.com>
2021-03-03 15:55:58 +0200

    introduce resolver helper

--- jm/src/resolver.rs
+++ jm/src/resolver.rs
@@ -6,7 +6,6 @@ use dashmap::DashMap;
 use dashmap::DashSet;
 use log::{debug, info};
 use semver::{Compat, Version, VersionReq};
-use std::collections::HashSet;
 use std::iter::FromIterator;
 use std::ops::Deref;
 use std::str::FromStr;
@@ -14,13 +13,17 @@ use std::str::FromStr;
 pub struct Resolver {
     cache: DashMap<String, DashSet<Package>>,
     fetcher: Fetcher,
+    helper: ResolverHelper,
 }
+
+struct ResolverHelper {}
 
 impl Resolver {
     pub fn new(fetcher: Fetcher) -> Resolver {
         Resolver {
             cache: DashMap::new(),
             fetcher,
+            helper: ResolverHelper::new(),
         }
     }
 
@@ -52,6 +55,7 @@ impl Resolver {
 
                 let set = DashSet::from_iter(vec![package.clone()].into_iter());
                 self.cache.insert(package_name.to_string(), set);
+
                 Ok(package)
             }
         }
@@ -71,18 +75,18 @@ impl Resolver {
             .fetcher
             .get_package_metadata(&dependency.real_name)
             .await?;
-        let version = get_package_exact_version(
-            requester,
-            &dependency.name,
-            &dependency.version_or_dist_tag,
-            &metadata,
-        );
 
-        let version_metadata = metadata.versions.get(&version).unwrap();
+        let package_requested_version = self
+            .helper
+            .extract_dependency_version_req(dependency, &metadata)?;
+
+        let version = self.helper.resolve_version(requester, &package_requested_version, &metadata)?;
+
+        let version_metadata = metadata.versions.get(&version.to_string()).unwrap();
 
         Ok(Package::new(
             dependency.name.to_string(),
-            version.clone(),
+            version.to_string(),
             Some(version_metadata.dependencies.clone()),
             None,
         ))
@@ -93,292 +97,74 @@ impl Resolver {
         package: &Package,
         dependency: &Dependency,
     ) -> Result<bool, String> {
-        info!(
-            "Fetching dependency {}@{}",
-            dependency.real_name, dependency.version_or_dist_tag
-        );
-
         let metadata = self
             .fetcher
             .get_package_metadata(&dependency.real_name)
             .await?;
 
-        let package_requested_version =
-            match VersionReq::parse_compat(&dependency.version_or_dist_tag, Compat::Npm) {
-                Ok(version) => version,
-                Err(_) => translate_dist_tag_to_version(
-                    &package.name,
-                    &dependency.version_or_dist_tag,
-                    &metadata,
-                ),
-            };
+        let package_requested_version = self
+            .helper
+            .extract_dependency_version_req(dependency, &metadata)?;
 
-        Ok(package_requested_version.matches(&Version::from_str(&package.version).unwrap()))
+        Ok(self
+            .helper
+            .version_matches(&package_requested_version, &package.version))
     }
 }
 
-fn translate_dist_tag_to_version(
-    package_name: &str,
-    requested_dist_tag: &str,
-    package_metadata: &PackageMetadata,
-) -> VersionReq {
-    let dist_tag = package_metadata
-        .dist_tags
-        .keys()
-        .find(|dist_tag| **dist_tag == requested_dist_tag.to_string())
-        .expect(&format!(
-            "Failed to resolve dist tag {} of package {}",
-            requested_dist_tag, package_name
-        ));
-
-    VersionReq::parse(package_metadata.dist_tags.get(dist_tag).unwrap()).unwrap()
-}
-
-fn filter_compatible_versions(
-    requested_version: &VersionReq,
-    package_metadata: &PackageMetadata,
-) -> HashSet<VersionReq> {
-    package_metadata
-        .versions
-        .keys()
-        .filter(|version| requested_version.matches(&Version::from_str(version).unwrap()))
-        .map(|version| VersionReq::parse_compat(version, Compat::Npm).unwrap())
-        .collect()
-}
-
-fn get_best_matching_version(versions: &HashSet<VersionReq>) -> String {
-    let mut sorted_versions = versions.into_iter().collect::<Vec<&VersionReq>>();
-    sorted_versions.sort();
-
-    sorted_versions.last().unwrap().to_string()
-}
-
-pub fn get_package_exact_version(
-    parent: &str,
-    package_name: &str,
-    version_or_dist_tag: &str,
-    package_metadata: &PackageMetadata,
-) -> String {
-    let package_requested_version =
-        match VersionReq::parse_compat(&version_or_dist_tag, Compat::Npm) {
-            Ok(version) => version,
-            Err(_) => translate_dist_tag_to_version(
-                &package_name,
-                &version_or_dist_tag,
-                &package_metadata,
-            ),
-        };
-
-    let compatible_versions =
-        filter_compatible_versions(&package_requested_version, &package_metadata);
-    // TODO: handle the case where no compatible_versions were found
-
-    if compatible_versions.len() == 0 {
-        panic!(
-            "No matching versions for {}->{} (requested {})",
-            parent, package_name, package_requested_version
-        )
+// TODO: test all helpers.
+impl ResolverHelper {
+    pub fn new() -> ResolverHelper {
+        ResolverHelper {}
     }
 
-    let version = get_best_matching_version(&compatible_versions);
-    let without_equal_prefix = &version[1..];
+    pub fn extract_dependency_version_req(
+        &self,
+        dependency: &Dependency,
+        package_metadata: &PackageMetadata,
+    ) -> Result<VersionReq, String> {
+        match VersionReq::parse_compat(&dependency.version_or_dist_tag, Compat::Npm) {
+            Ok(version) => Ok(version),
+            Err(_) => {
+                let dist_tag = package_metadata
+                    .dist_tags
+                    .get(&dependency.version_or_dist_tag)
+                    .ok_or(format!(
+                        "Failed to resolve dist tag {} of package {}",
+                        &dependency.version_or_dist_tag, &dependency.real_name
+                    ))?;
 
-    String::from(without_equal_prefix)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::npm::{PackageMetadata, VersionMetadata};
-    use maplit::hashmap;
-    use std::collections::HashMap;
-
-    fn with_package_metadata() -> PackageMetadata {
-        PackageMetadata {
-            package_name: "some-package".to_string(),
-            dist_tags: hashmap! {
-                "latest".to_string() => "2.0.0".to_string(),
-            },
-            versions: hashmap! {
-                "1.0.1".to_string() => VersionMetadata {
-                    shasum: "some-shasum".to_string(),
-                    tarball: "some-tarball".to_string(),
-                    dependencies: HashMap::new(),
-                },
-                "1.1.0".to_string() => VersionMetadata {
-                    shasum: "some-shasum".to_string(),
-                    tarball: "some-tarball".to_string(),
-                    dependencies: HashMap::new(),
-                },
-                "2.0.0".to_string() => VersionMetadata {
-                    shasum: "some-shasum".to_string(),
-                    tarball: "some-tarball".to_string(),
-                    dependencies: HashMap::new(),
-                },
-            },
+                Ok(VersionReq::parse(dist_tag).unwrap())
+            }
         }
     }
 
-    #[test]
-    fn filter_compatible_versions_tilde() {
-        let requested_version = VersionReq::parse_compat("~1.0.0", Compat::Npm).unwrap();
-        let packages_metadata = with_package_metadata();
+    pub fn version_matches(&self, requested_version: &VersionReq, version: &str) -> bool {
+        requested_version.matches(&Version::from_str(version).unwrap())
+    }
 
-        let result = filter_compatible_versions(&requested_version, &packages_metadata);
-        let expected = vec![VersionReq::parse_compat("1.0.1", Compat::Npm).unwrap()]
-            .into_iter()
+    pub fn resolve_version(
+        &self,
+        parent: &str,
+        requested_version: &VersionReq,
+        package_metadata: &PackageMetadata,
+    ) -> Result<Version, String> {
+        let mut matching_versions: Vec<Version> = package_metadata
+            .versions
+            .keys()
+            .filter(|version| self.version_matches(requested_version, version))
+            .map(|version| Version::from_str(version).unwrap())
             .collect();
 
-        assert_eq!(result, expected);
-    }
+        if matching_versions.is_empty() {
+            return Err(format!(
+                "No matching versions for {}->{} (requested {})",
+                parent, package_metadata.package_name, requested_version
+            ));
+        }
 
-    #[test]
-    fn filter_compatible_versions_carrot() {
-        let requested_version = VersionReq::parse_compat("^1.0.0", Compat::Npm).unwrap();
-        let packages_metadata = with_package_metadata();
+        matching_versions.sort();
 
-        let result = filter_compatible_versions(&requested_version, &packages_metadata);
-        let expected = vec![
-            VersionReq::parse_compat("1.0.1", Compat::Npm).unwrap(),
-            VersionReq::parse_compat("1.1.0", Compat::Npm).unwrap(),
-        ]
-        .into_iter()
-        .collect();
-
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn filter_compatible_versions_asterisks() {
-        let requested_version = VersionReq::parse_compat("*", Compat::Npm).unwrap();
-        let packages_metadata = with_package_metadata();
-
-        let result = filter_compatible_versions(&requested_version, &packages_metadata);
-        let expected = vec![
-            VersionReq::parse_compat("2.0.0", Compat::Npm).unwrap(),
-            VersionReq::parse_compat("1.1.0", Compat::Npm).unwrap(),
-            VersionReq::parse_compat("1.0.1", Compat::Npm).unwrap(),
-        ]
-        .into_iter()
-        .collect();
-
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn filter_compatible_versions_gt() {
-        let requested_version = VersionReq::parse_compat(">1.0.1", Compat::Npm).unwrap();
-        let packages_metadata = with_package_metadata();
-
-        let result = filter_compatible_versions(&requested_version, &packages_metadata);
-        let expected = vec![
-            VersionReq::parse_compat("2.0.0", Compat::Npm).unwrap(),
-            VersionReq::parse_compat("1.1.0", Compat::Npm).unwrap(),
-        ]
-        .into_iter()
-        .collect();
-
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn filter_compatible_versions_ge() {
-        let requested_version = VersionReq::parse_compat(">=1.0.1", Compat::Npm).unwrap();
-        let packages_metadata = with_package_metadata();
-
-        let result = filter_compatible_versions(&requested_version, &packages_metadata);
-        let expected = vec![
-            VersionReq::parse_compat("2.0.0", Compat::Npm).unwrap(),
-            VersionReq::parse_compat("1.1.0", Compat::Npm).unwrap(),
-            VersionReq::parse_compat("1.0.1", Compat::Npm).unwrap(),
-        ]
-        .into_iter()
-        .collect();
-
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn filter_compatible_versions_lt() {
-        let requested_version = VersionReq::parse_compat("<2.0.0", Compat::Npm).unwrap();
-        let packages_metadata = with_package_metadata();
-
-        let result = filter_compatible_versions(&requested_version, &packages_metadata);
-        let expected = vec![
-            VersionReq::parse_compat("1.0.1", Compat::Npm).unwrap(),
-            VersionReq::parse_compat("1.1.0", Compat::Npm).unwrap(),
-        ]
-        .into_iter()
-        .collect();
-
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn filter_compatible_versions_le() {
-        let requested_version = VersionReq::parse_compat("<=2.0.0", Compat::Npm).unwrap();
-        let packages_metadata = with_package_metadata();
-
-        let result = filter_compatible_versions(&requested_version, &packages_metadata);
-        let expected = vec![
-            VersionReq::parse_compat("2.0.0", Compat::Npm).unwrap(),
-            VersionReq::parse_compat("1.1.0", Compat::Npm).unwrap(),
-            VersionReq::parse_compat("1.0.1", Compat::Npm).unwrap(),
-        ]
-        .into_iter()
-        .collect();
-
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn filter_compatible_versions_exact() {
-        let requested_version = VersionReq::parse_compat("1.0.1", Compat::Npm).unwrap();
-        let packages_metadata = with_package_metadata();
-
-        let result = filter_compatible_versions(&requested_version, &packages_metadata);
-        let expected = vec![VersionReq::parse_compat("1.0.1", Compat::Npm).unwrap()]
-            .into_iter()
-            .collect();
-
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn filter_compatible_versions_no_match() {
-        let requested_version = VersionReq::parse_compat("1.0.3", Compat::Npm).unwrap();
-        let packages_metadata = with_package_metadata();
-
-        let result = filter_compatible_versions(&requested_version, &packages_metadata);
-        let expected = HashSet::new();
-
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn translate_dist_tag_to_version_exists() {
-        let package_metadata = with_package_metadata();
-
-        let result = translate_dist_tag_to_version(
-            &package_metadata.package_name,
-            "latest",
-            &package_metadata,
-        );
-
-        assert_eq!(result, VersionReq::from_str("2.0.0").unwrap());
-    }
-
-    #[test]
-    #[should_panic(
-        expected = "Failed to resolve dist tag non-existing-dist-tag of package some-package"
-    )]
-    fn translate_dist_tag_to_version_does_not_exist() {
-        let package_metadata = with_package_metadata();
-
-        translate_dist_tag_to_version(
-            &package_metadata.package_name,
-            "non-existing-dist-tag",
-            &package_metadata,
-        );
+        Ok(matching_versions.into_iter().last().unwrap())
     }
 }
