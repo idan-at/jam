5fa68b03cd5913a79a742413236113e080805c5a
Idan Attias <idana@wix.com>
2021-03-16 12:09:12 +0200

    add missing resolver helper tests

--- jm-core/src/package.rs
+++ jm-core/src/package.rs
@@ -124,13 +124,16 @@ mod tests {
         let npm_package = Package::Package(NpmPackage::new(
             String::from("some-npm-package"),
             String::from("1.0.0"),
-            None, None,
+            None,
+            None,
         ));
 
         let workspace_package = Package::WorkspacePackage(WorkspacePackage::new(
             String::from("some-workspace-package"),
             String::from("2.0.0"),
-            None, None, PathBuf::new()
+            None,
+            None,
+            PathBuf::new(),
         ));
 
         assert_eq!(npm_package.name(), "some-npm-package");
@@ -179,7 +182,7 @@ mod tests {
             Some(hashmap! {
                 "lol".to_string() => "npm:lodash@~2.0.0".to_string()
             }),
-            PathBuf::new()
+            PathBuf::new(),
         ));
 
         let expected = vec![
@@ -197,7 +200,6 @@ mod tests {
 
         assert_eq!(package.dependencies(), expected);
     }
-
 
     #[test]
     fn npm_package_uses_dependencies_over_dev_dependencies_in_case_of_repetitions() {
@@ -232,7 +234,7 @@ mod tests {
             Some(hashmap! {
                 "lodash".to_string() => "~2.0.0".to_string()
             }),
-            PathBuf::new()
+            PathBuf::new(),
         ));
 
         let expected = vec![Dependency {
--- jm/src/resolver.rs
+++ jm/src/resolver.rs
@@ -123,7 +123,6 @@ impl PackageResolver for Resolver {
     }
 }
 
-// TODO: test all helpers.
 impl ResolverHelper {
     pub fn new() -> ResolverHelper {
         ResolverHelper {}
@@ -137,7 +136,7 @@ impl ResolverHelper {
         match VersionReq::parse_compat(&dependency.version_or_dist_tag, Compat::Npm) {
             Ok(version) => Ok(version),
             Err(_) => {
-                let dist_tag = package_metadata
+                let version = package_metadata
                     .dist_tags
                     .get(&dependency.version_or_dist_tag)
                     .ok_or(format!(
@@ -145,7 +144,14 @@ impl ResolverHelper {
                         &dependency.version_or_dist_tag, &dependency.real_name
                     ))?;
 
-                Ok(VersionReq::parse(dist_tag).unwrap())
+                if package_metadata.versions.contains_key(version) {
+                    Ok(VersionReq::parse(version).unwrap())
+                } else {
+                    Err(format!(
+                        "{}@{} points to version {}, which does not exist",
+                        &dependency.real_name, &dependency.version_or_dist_tag, &version
+                    ))
+                }
             }
         }
     }
@@ -177,5 +183,190 @@ impl ResolverHelper {
         matching_versions.sort();
 
         Ok(matching_versions.into_iter().last().unwrap())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::npm::VersionMetadata;
+    use maplit::hashmap;
+    use std::collections::HashMap;
+
+    #[test]
+    fn extract_dependency_version_req_dist_tag_ok() {
+        let helper = ResolverHelper::new();
+        let package_name = "dep1".to_string();
+        let version = "1.0.0".to_string();
+        let dist_tag = "beta".to_string();
+
+        let dependency = Dependency {
+            name: package_name.clone(),
+            real_name: package_name.clone(),
+            version_or_dist_tag: dist_tag.clone(),
+        };
+
+        let metadata = PackageMetadata {
+            package_name: package_name.clone(),
+            dist_tags: hashmap! {
+                dist_tag.clone() => version.clone()
+            },
+            versions: hashmap! {
+                version.clone() => VersionMetadata {
+                    shasum: "a-shasum".to_string(),
+                    tarball: "a-tarball".to_string(),
+                    dependencies: HashMap::new(),
+                }
+            },
+        };
+
+        let result = helper.extract_dependency_version_req(&dependency, &metadata);
+
+        assert_eq!(result, Ok(VersionReq::from_str(&version).unwrap()));
+    }
+
+    #[test]
+    fn extract_dependency_version_req_dist_tag_not_found() {
+        let helper = ResolverHelper::new();
+        let package_name = "dep1".to_string();
+        let version = "1.0.0".to_string();
+        let dist_tag = "beta".to_string();
+
+        let dependency = Dependency {
+            name: package_name.clone(),
+            real_name: package_name.clone(),
+            version_or_dist_tag: dist_tag.clone(),
+        };
+
+        let metadata = PackageMetadata {
+            package_name: package_name.clone(),
+            dist_tags: hashmap! {
+                "not-beta".to_string() => version.clone()
+            },
+            versions: hashmap! {
+                version.clone() => VersionMetadata {
+                    shasum: "a-shasum".to_string(),
+                    tarball: "a-tarball".to_string(),
+                    dependencies: HashMap::new(),
+                }
+            },
+        };
+
+        let result = helper.extract_dependency_version_req(&dependency, &metadata);
+
+        assert_eq!(
+            result,
+            Err(format!(
+                "Failed to resolve dist tag {} of package {}",
+                dist_tag, package_name
+            ))
+        );
+    }
+
+    #[test]
+    fn extract_dependency_version_req_dist_tag_no_matching_version() {
+        let helper = ResolverHelper::new();
+        let package_name = "dep1".to_string();
+        let version = "1.0.0".to_string();
+        let dist_tag = "beta".to_string();
+
+        let dependency = Dependency {
+            name: package_name.clone(),
+            real_name: package_name.clone(),
+            version_or_dist_tag: dist_tag.clone(),
+        };
+
+        let metadata = PackageMetadata {
+            package_name: package_name.clone(),
+            dist_tags: hashmap! {
+                dist_tag.to_string() => version.clone()
+            },
+            versions: hashmap! {
+                "2.0.0".to_string() => VersionMetadata {
+                    shasum: "a-shasum".to_string(),
+                    tarball: "a-tarball".to_string(),
+                    dependencies: HashMap::new(),
+                }
+            },
+        };
+
+        let result = helper.extract_dependency_version_req(&dependency, &metadata);
+
+        assert_eq!(
+            result,
+            Err(format!(
+                "{}@{} points to version 1.0.0, which does not exist",
+                package_name, dist_tag
+            ))
+        );
+    }
+
+    #[test]
+    fn version_matches() {
+        let helper = ResolverHelper::new();
+        let version_req = VersionReq::parse("~1.0.0").unwrap();
+
+        assert!(helper.version_matches(&version_req, "1.0.0"));
+        assert!(!helper.version_matches(&version_req, "2.0.0"));
+    }
+
+    #[test]
+    fn resolve_version_finds_the_best_match() {
+        let helper = ResolverHelper::new();
+        let version_req = VersionReq::parse("~1.0.0").unwrap();
+        let metadata = PackageMetadata {
+            package_name: "a-package".to_string(),
+            dist_tags: HashMap::new(),
+            versions: hashmap! {
+                "1.0.0".to_string() => VersionMetadata {
+                    shasum: "a-shasum".to_string(),
+                    tarball: "a-tarball".to_string(),
+                    dependencies: HashMap::new(),
+                },
+                "1.0.1".to_string() => VersionMetadata {
+                    shasum: "a-shasum".to_string(),
+                    tarball: "a-tarball".to_string(),
+                    dependencies: HashMap::new(),
+                },
+                "2.0.0".to_string() => VersionMetadata {
+                    shasum: "a-shasum".to_string(),
+                    tarball: "a-tarball".to_string(),
+                    dependencies: HashMap::new(),
+                }
+            },
+        };
+
+        let result = helper.resolve_version("never-mind", &version_req, &metadata);
+
+        assert_eq!(result, Ok(Version::from_str("1.0.1").unwrap()));
+    }
+
+    #[test]
+    fn resolve_version_error_when_no_matching_versions_exist() {
+        let helper = ResolverHelper::new();
+        let version_req = VersionReq::parse("~3.0.0").unwrap();
+        let parent = "parent-package".to_string();
+        let package_name = "package".to_string();
+        let metadata = PackageMetadata {
+            package_name: package_name.clone(),
+            dist_tags: HashMap::new(),
+            versions: hashmap! {
+                "1.0.0".to_string() => VersionMetadata {
+                    shasum: "a-shasum".to_string(),
+                    tarball: "a-tarball".to_string(),
+                    dependencies: HashMap::new(),
+                }
+            },
+        };
+
+        let result = helper.resolve_version(&parent, &version_req, &metadata);
+
+        assert_eq!(
+            result,
+            Err(format!(
+                "No matching versions for {}->{} (requested {})",
+                parent, package_name, version_req
+            ))
+        );
     }
 }
