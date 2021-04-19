d70c037afeeb5e163ac43a49416da30070ed5a9c
Idan Attias <idana@wix.com>
2021-04-19 16:19:58 +0300

    handle @types/node

--- jam/src/archiver.rs
+++ jam/src/archiver.rs
@@ -3,9 +3,13 @@ use flate2::read::GzDecoder;
 use std::fs;
 use std::fs::File;
 use std::path::Path;
-use tar::Archive;
+use std::path::PathBuf;
+use tar::{Archive, EntryType};
 
+// Default npm pack directory
 const NPM_PACK_PATH_PREFIX: &'static str = "package";
+// @types/node pack directory
+const TYPES_NODE_PATH_PREFIX: &'static str = "node";
 
 pub trait Archiver: Send + Sync {
     fn extract_to(&self, tarball_path: &Path, target_path: &Path) -> Result<(), JamError>;
@@ -18,6 +22,18 @@ impl DefaultArchiver {
     pub fn new() -> DefaultArchiver {
         DefaultArchiver {}
     }
+
+    fn strip_known_prefixes(&self, path: &Path) -> PathBuf {
+        let sanitized_path = match path.strip_prefix(NPM_PACK_PATH_PREFIX) {
+            Ok(stripped_path) => stripped_path.to_owned(),
+            Err(_) => path.to_path_buf(),
+        };
+
+        match sanitized_path.strip_prefix(TYPES_NODE_PATH_PREFIX) {
+            Ok(final_path) => final_path.to_owned(),
+            Err(_) => sanitized_path,
+        }
+    }
 }
 
 impl Archiver for DefaultArchiver {
@@ -27,16 +43,12 @@ impl Archiver for DefaultArchiver {
         let mut archive = Archive::new(tar);
 
         for mut entry in archive.entries()?.filter_map(|e| e.ok()) {
-            let entry_path = entry.path()?;
-
-            if entry_path.to_str() == Some(NPM_PACK_PATH_PREFIX) {
+            if entry.header().entry_type() == EntryType::Directory {
                 continue;
             }
 
-            let file_inner_path = match entry_path.strip_prefix(NPM_PACK_PATH_PREFIX) {
-                Ok(stripped_path) => stripped_path.to_owned(),
-                Err(_) => entry_path.to_path_buf(),
-            };
+            let entry_path = entry.path()?;
+            let file_inner_path = self.strip_known_prefixes(&entry_path);
 
             let file_path = target_path.join(&file_inner_path);
             fs::create_dir_all(&file_path.parent().unwrap())?;
@@ -66,6 +78,22 @@ mod tests {
 
         let fixtures_path = get_fixtures_path();
         let archive_path = fixtures_path.join("simple.tgz");
+
+        let expected_file_path = tmp_dir.path().join("package.json");
+
+        let result = archiver.extract_to(&archive_path, tmp_dir.path());
+
+        assert!(result.is_ok());
+        assert!(expected_file_path.exists());
+    }
+
+    #[test]
+    fn extracts_archive_to_given_path_while_ignoring_a_unique_path_prefix() {
+        let archiver = DefaultArchiver::new();
+        let tmp_dir = TempDir::new("jam-archiver").unwrap();
+
+        let fixtures_path = get_fixtures_path();
+        let archive_path = fixtures_path.join("different_path_prefix.tgz");
 
         let expected_file_path = tmp_dir.path().join("package.json");
 
